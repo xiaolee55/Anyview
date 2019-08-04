7e867d7d55c644dc46d98f4607cfe878ec90bfa4
1084235321@qq.com <1084235321@qq.com>
2019-08-04 11:34:56 +0800

    增加登录验证功能

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -3,12 +3,12 @@
        <canvas id="canvas"></canvas>
         <el-form ref="form"  label-width="80px">
             <h1 style="color: #409eff">Anyview</h1>
-            <el-form-item>
-                    <el-radio-group v-model="user.roleId">
-                    <el-radio-button label="1">管理员</el-radio-button>
-                    <el-radio-button label="2">教师</el-radio-button>
-                    <el-radio-button label="3">学生</el-radio-button>
-                    </el-radio-group>
+            <el-form-item class="role">
+                <el-radio-group v-model="user.roleId">
+                  <el-radio-button label="1">管理员</el-radio-button>
+                  <el-radio-button label="2">教师</el-radio-button>
+                  <el-radio-button label="3">学生</el-radio-button>
+                </el-radio-group>
             </el-form-item>
             <el-form-item label="账号">
                 <el-input v-model="user.username" placeholder="请输入账号"></el-input>
@@ -17,13 +17,23 @@
             <el-input v-model="user.password" placeholder="请输入密码" show-password></el-input>
             </el-form-item>
             <el-form-item >
-                <el-select v-model="user.schoolId" placeholder="请选择您的学校" >
-                <el-option v-for="(school,index) in schoolList" :key="index" :value="school.id">{{school.schoolName}}</el-option>
+                <el-select v-model="user.schoolId" placeholder="请选择您的学校">
+                  <el-option v-for="(school,index) in schoolList" :key="index" :value="school.id" :label="school.schoolName"></el-option>
                 </el-select>
                 <el-checkbox v-model="user.switch">记住密码</el-checkbox>
             </el-form-item>
-            <el-form-item>
-                <el-button type="primary" @click.once="submit">登录</el-button>
+            <transition name="login">
+              <el-alert
+                title="登录失败"
+                type="error"
+                :description="errDesc"
+                show-icon
+                v-if='!error'
+                style="margin-bottom:22px">
+              </el-alert>
+             </transition>
+            <el-form-item class="butt">
+                <el-button type="primary" @click="submit">登录</el-button>
                 <el-button type="danger">忘记密码</el-button>
             </el-form-item>
         </el-form>
@@ -34,7 +44,8 @@
 export default {
   data () {
     return {
-      form: {},
+      error: true,
+      errDesc: '',
       user: {
         roleId: 3,
         username: '',
@@ -47,25 +58,46 @@ export default {
     }
   },
   mounted () {
-      console.log(this.socket.sendSock({type: 6,content: '获取学校列表'},this.getSchool));
+      this.socket.sendSock({type: 6,content: '获取学校列表'},this.getSchool)
     },
   methods: {
-      getSchool (schoolList) {
-          this.schoolList = schoolList.content 
+      getSchool (e) {
+          this.schoolList = e.content 
       },
-      getUserInfo (userInfo) {
-          //拿到学生信息
-          this.userInfo = userInfo.content
-          //路由跳转并传递信息
-          this.$router.push({ 
-            name: 'workSelect',
-           query:{ 
-                user: JSON.stringify(this.userInfo)
-            },
-        })
+      getUserInfo (e) {
+          if(e.type==-12){  //账号不存在
+            this.error=false
+            this.errDesc='信息输入错误'
+            return
+          }
+          else{
+            this.$message({
+              showClose: true,
+              message: '欢迎您,'+e.content.name,
+              type: 'success'
+            });
+            this.userInfo = e.content
+            this.$router.push({   // 路由跳转并传递信息
+              name: 'workSelect',
+              query:{ 
+                  user: JSON.stringify(this.userInfo)
+              },
+          })
+        }
       },
-      submit () {
-          this.socket.sendSock({type: 0, content: this.user}, this.getUserInfo)
+      submit () {       
+        for(let item in this.user){   //检查输入信息是否为空
+          if(this.user[item]===''){
+            switch(item){
+              case 'username': this.errDesc='账号输入为空';break;
+              case 'password': this.errDesc='密码输入为空';break;
+              case 'schoolId': this.errDesc='未选择学校';break;
+            }
+            this.error=false
+            return
+          }
+        }
+        this.socket.sendSock({type: 0, content: this.user}, this.getUserInfo)
       }
 
   }
@@ -92,5 +124,24 @@ html{
                 background-color: rgba(255,255,255,0.5);
                 border-radius: 8%;
                 box-shadow: 0 0 12px 1px grey;
+                transition: height .5s .5s;
+}
+/* 过渡效果开始 */
+.login-enter-active, .login-leave-active {
+  transition: opacity .5s;
+}
+.login-enter, .login-leave-to /* .fail_login-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+/* 过渡效果结束 */
+
+/* 让选择栏和按钮居中 */
+.role .el-form-item__content{
+  margin-left: 0!important;
+  text-align: center;
+}
+.butt .el-form-item__content{
+  margin-left: 0!important;
+  text-align: center;
 }
 </style>
